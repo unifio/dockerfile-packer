f3924be167a28e3ae7bafe3724e298158d711407
Kathy Jonick <jonick@unif.io>
2017-09-20 11:10:03 -0700

    Skip 1.0.3 - there was a bug with ansible-remote (GH-5146)

--- uat/serverspec/spec_helper.rb
+++ uat/serverspec/spec_helper.rb
@@ -6,7 +6,7 @@ options[:user] = ENV['TARGET_USER']
 options[:keys] = ENV['TARGET_KEY']
 options[:host_name] = ENV['TARGET_HOST']
 options[:port] = ENV['TARGET_PORT']
-options[:verify_host_key] = false unless ENV['SERVERSPEC_HOST_KEY_CHECKING'] =~ (/^(true|t|yes|y|1)$/i)
+options[:paranoid] = false unless ENV['SERVERSPEC_HOST_KEY_CHECKING'] =~ (/^(true|t|yes|y|1)$/i)
 
 set :host,         options[:host_name]
 set :ssh_options,  options
